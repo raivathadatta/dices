0bef9cd9f5c17472de880e7b18f9e668cc7b8e9e
raivatha datta <raivatha.28.2@mountblue.tech>
2024-06-14 15:58:14 +0530

    added changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
-// import reactLogo fromreact.svg'
-// import viteLogo from '/vite.svg'
+
 import './App.css'
 
+let imageArray = ['one.png', 'two.png', 'three.png', 'four.png', 'five.png', 'six.png'];
+
 function App() {
-  let [imageIndex, setDiceOne] = useState(0)
-  let [imageIndexDice2, setDiceTwo] = useState(0)
-  let imageArray = ['one.png', 'two.png', 'three.png', 'four.png', 'five.png', 'six.png'];
-  console.log(imageArray[imageIndex])
-  let whoWon = imageIndex > imageIndexDice2 ? 'Player 1 Wins' : imageIndex < imageIndexDice2 ? 'Player 2 Wins' : 'Tie'
+  let [dices, setDiceOne] = useState({ dices1: 0, dices2: 0 })
+
+  let whoWon = dices.dices1 > dices.dices2 ? 'Player 1 Wins' : dices.dices1 < dices.dices2 ? 'Player 2 Wins' : 'Tie'
 
 
   function changeImage() {
-    // console.log(Math.floor((Math.random() * 10)%6))
-    setDiceOne(Math.floor((Math.random() * 10)%6))
-    setDiceTwo(Math.floor((Math.random() * 10)%6))
 
-    
+    let dices1 = Math.floor((Math.random() * 10) % 6)
+    let dices2 = Math.floor((Math.random() * 10) % 6)
+
+    setDiceOne({ dices1, dices2 })
+
   }
 
 
@@ -25,14 +25,14 @@ function App() {
       <div className='w-[50%] h-[50%] bg-black  mt[10%] p-4 text-red-500'>
         <h1 className='text-[40px] font-black '>{whoWon}</h1>
         <div className=' flex flex-row justify-between' >
-     
           <div className="w-[50%] ">
-          <h1 className='text-[30px] font-bold'>Player 1</h1>
-            <img className='w-[100%] h-[100%] transform transition duration-300 hover:rotate-90' src={imageArray[imageIndex]} />
+            <h1 className='text-[30px] font-bold'>Player 1</h1>
+            <img className='w-[100%] h-[100%] transform transition duration-300 hover:rotate-90' src={imageArray[dices.dices1]} />
           </div>
           <div className=" w-[50%]">
             <h1 className='text-[30px] font-bold'>Player 2</h1>
-            <img className='w-[100%] h-[100%] transform transition duration-300 hover:rotate-90' src={imageArray[imageIndexDice2]} />       </div>
+            <img className='w-[100%] h-[100%] transform transition duration-300 hover:rotate-90' src={imageArray[dices.dices2]} />
+          </div>
 
         </div>
         <button className='w-[20%] h-[20%] bg-red-500 p-5 mt-[8%] font-bold text-black' onClick={changeImage}>Play</button>
